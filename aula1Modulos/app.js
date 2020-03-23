46640ec6171d01c1040cdd69aaa353d2bbff2e3a
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-03-23 02:06:14 -0300

    aula 23/03

---
+++ aula1Modulos/app.js
@@ -0,0 +1,2 @@
+const mod1 = require('./mod1').falaNome;
+console.log(mod1.falaNome())
---
+++ aula1Modulos/mod1.js
@@ -0,0 +1,8 @@
+const nome = 'Rodrigo'
+const sobrenome = 'Tanaka'
+
+const falaNome = () => nome + ' ' + sobrenome
+
+exports.nome = nome;
+exports.sobrenome = sobrenome;
+exports.falaNome = falaNome;
